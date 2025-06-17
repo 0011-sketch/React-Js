8af57f50c995ada4d59330eda3a919b0d99951c4
0011-sketch <makineninaveen121@gmail.com>
2025-06-17 10:17:58 +0000

    3rd way of terinary operator

--- vite-project/src/components/products/index.jsx
+++ vite-project/src/components/products/index.jsx
@@ -8,23 +8,21 @@ function ProductList({ name, city, ListOfProducts }) {
   //   const { name, city } = props;
   //   console.log(name);
 
-const flag = true;
+  const flag = false;
 
- function renderTextBlock(getFlag){
-  return getFlag? <h4>
-        Name is {name} he is from belongs to city {city}
-      </h4>:<h4>Hello</h4>
- }
- 
+  const renderTextBlock = flag ? <h4>
+    Name is {name} he is from belongs to city {city}
+  </h4> : <h4>Hello</h4>
+
 
   return (
     <div>
       <h3 className="title">Ecommerce Project</h3>
       {/* <ProductItem /> */}
       {/* terinary operator we need to use the curly brases  beacouse it is a dynamic values */}
+      {/* for function we need to cal the meathod but here we are storing in a variable we no need to call it */}
+      {renderTextBlock}
 
-      {renderTextBlock(flag)}
-     
       <ul>
         {ListOfProducts.map((item, index) => (
           <ProductItem singleProductItem={item} key={index} />
