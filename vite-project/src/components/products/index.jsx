8e7b45abf538e102c189d901aa3b026c1fb80fb9
0011-sketch <makineninaveen121@gmail.com>
2025-06-17 14:19:56 +0000

    1.useSate

--- vite-project/src/components/products/index.jsx
+++ vite-project/src/components/products/index.jsx
@@ -1,27 +1,32 @@
+import { useState } from "react";
 import ProductItem from "./component/product-item";
 import "./style.css";
 
-// note: in normal html,css we give the class but in react js we need to use the className for class based components we use the class keyword
-// so in function component if we use the  className it wont conflict with class name
+const initialState = true;
 
 function ProductList({ name, city, ListOfProducts }) {
-  //   const { name, city } = props;
-  //   console.log(name);
 
-  const flag = false;
+  const [flag, setFlag] = useState(initialState)
+  // this initial value can be anything 
+  // it can be null 
+  // it can be[]
+  // it can be { }
+  // it can be ""
+  // it can be 10
+  // it can be a boolean
 
-  const renderTextBlock = flag ? <h4>
-    Name is {name} he is from belongs to city {city}
-  </h4> : <h4>Hello</h4>
 
+  function handleToggleText() {
+    setFlag(!flag)
+  }
 
   return (
     <div>
       <h3 className="title">Ecommerce Project</h3>
-      {/* <ProductItem /> */}
-      {/* terinary operator we need to use the curly brases  beacouse it is a dynamic values */}
-      {/* for function we need to cal the meathod but here we are storing in a variable we no need to call it */}
-      {renderTextBlock}
+      <button onClick={handleToggleText}>Toggle Text</button>
+      {
+        flag ? <h4>{name} and {city}</h4> : <h4>Hello world</h4>
+      }
 
       <ul>
         {ListOfProducts.map((item, index) => (
