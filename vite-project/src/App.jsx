31b203e590de8e64455ec482c17ef7fa49290660
0011-sketch <makineninaveen121@gmail.com>
2025-06-17 14:32:54 +0530

    1.only used css

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,9 @@
 // import ArrowFunction from "./components/arrowFunction";
 // import FunctionalComponent from "./components/functionalComponent";
 
+//styling react js
+// Css frameworks like [material Ui,Ant design, tailwind css,section ui]
+import "./App.css";
 import ProductList from "./components/products";
 const dummyProductData = ["Product 1", "Product 2", "product 3"];
 
--- vite-project/src/components/products/index.jsx
+++ vite-project/src/components/products/index.jsx
@@ -1,11 +1,15 @@
 import ProductItem from "./component/product-item";
+import "./style.css";
+
+// note: in normal html,css we give the class but in react js we need to use the className for class based components we use the class keyword
+// so in function component if we use the  className it wont conflict with class name
 
 function ProductList({ name, city, ListOfProducts }) {
   //   const { name, city } = props;
   //   console.log(name);
   return (
     <div>
-      <h3>Ecommerce Project</h3>
+      <h3 className="title">Ecommerce Project</h3>
       {/* <ProductItem /> */}
       <h4>
         Name is {name} he is from belongs to city {city}
